bf573f175cc65f197b4a0b7b19a0f6efa794d062
Junpeng <2833551728@qq.com>
2019-05-28 22:43:57 +0800

    first commit

---
+++ src/study01/HallowWorld.java
@@ -0,0 +1,11 @@
+package study01;
+
+public class HallowWorld {
+    public static void main(String[] args) {
+//        String a = "adsdfdsafs";
+//        Scanner scanner = new Scanner(System.in);
+//        String b = new String("adsdfdsafs");
+        String[][] abc = new String[2][2];
+        System.out.println(abc[1][1]);
+    }
+}
